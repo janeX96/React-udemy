d40c3f2564d8f13ecf0acb71ae05751a3d39e555
s16503 <s16503@pjwstk.edu.pl>
2021-09-07 19:23:39 +0200

    3-12 mini app "add sign"

--- App.js
+++ App.js
@@ -1,40 +1,29 @@
-class ShoppingList extends React.Component{
+//Przycisk, po kliknięciu dodawana jest litera do tekstu
+
+class App extends React.Component{
 
   state = {
-    items1: 'ogórki',
-    items2: 'sok',
-    items3: 'wódka'
+    text: ""
+  }
+
+  handleClick = () => {
+    // this.state.text += "A";
+    // console.log(this.state.text)
+
+    const letter = "A"
+    this.setState({
+      text: this.state.text+letter
+    })
   }
 
   render(){
     return(
       <>
-        <h1>Lista zakupów</h1>
-        <ul>
-          <ItemList name={this.state.items1} example={2+2}/>
-          <ItemList name={this.state.items2}/>
-          <ItemList name={this.state.items3}/>
-        </ul>
+      <button onClick={this.handleClick}>Dodaj "A"</button>
+      <h1>{this.state.text}</h1>
       </>
     )
   }
 }
 
-// const ItemList = (props) => {
-
-//   return(
-//     <li>{props.name} - {props.example}</li>
-//   )
-// }
-
-class ItemList extends React.Component{
-
-  render(){
-    return(
-        <li>{this.props.name} - {this.props.example}</li>
-      )
-  }
-
-}
-
-ReactDOM.render(<ShoppingList/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
