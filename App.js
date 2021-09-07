26bf07c9bf4f4d9a1a7d2e62d3eda126d4b32e04
s16503 <s16503@pjwstk.edu.pl>
2021-09-07 19:48:46 +0200

    3-14 bind, setState, konstruktor

--- App.js
+++ App.js
@@ -2,24 +2,38 @@
 
 class App extends React.Component{
 
+  // constructor(props){
+  //     super(props);
+  //     this.state = {
+  //       text: ""
+  //     }
+  //     this.handleClick = this.handleClick.bind(this)
+  // }
+
   state = {
     text: ""
   }
 
-  handleClick = () => {
+  handleClick = () =>{
     // this.state.text += "A";
     // console.log(this.state.text)
 
     const letter = "A"
-    this.setState({
-      text: this.state.text+letter
+    // this.setState({
+    //   text: this.state.text+letter
+    // })
+
+    this.setState(() => ({
+        text: this.state.text + letter
     })
+    )
+
   }
 
   render(){
     return(
       <>
-      <button onClick={this.handleClick}>Dodaj "A"</button>
+      <button onClick={this.handleClick.bind(this)}>Dodaj "A"</button>
       <h1>{this.state.text}</h1>
       </>
     )
