c162145f3197c6f9b1c7a876e208e0bb45fcb78b
s16503 <s16503@pjwstk.edu.pl>
2021-09-08 13:08:52 +0200

    3-19 zliczanie kliknięć i wysw wyniku

--- App.js
+++ App.js
@@ -1,38 +1,47 @@
-class Message extends React.Component {
+class Counter extends React.Component{
 
-  constructor(props){
-    super(props)
-    this.state = {
-      messageIsActive: false
-    }
-
-    this.handleMessageButton = this.handleMessageButton.bind(this)
+  state={
+    count: 0,
+    result: this.props.result
   }
 
-
-  handleMessageButton(){
-    this.setState({
-      messageIsActive: !this.state.messageIsActive
-    })
+  handleMathClick(type, number = 1){
+    
+    if(type === "substraction"){
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+        result: prevState.result - number
+      }))
+    }else if(type ==="reset"){
+      this.setState(prevState => (
+        {
+          count: prevState.count + 1,
+          result: 0
+        }
+      ))
+    }else if(type === "addition"){
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+        result: prevState.result + number
+    }))
+   }
   }
+  
 
   render(){
-    console.log(this.state.messageIsActive)
-    const text = "sadasasdasds asdsa asdsa sadsdasd ad sad sa sadasasdasds asdsa asdsa sadsdasd ad sad sa. sadasasdasds asdsa asdsa sadsdasd ad sad sa."
     return(
       <>
-        <button onClick={this.handleMessageButton}>{this.state.messageIsActive ? "Ukryj" : "Pokaż"}</button>
-        {/* {this.state.messageIsActive ? <p>{text}</p> : null} */}
-        {this.state.messageIsActive && <p>{text}</p>}
+        <button onClick={this.handleMathClick.bind(this,"substraction", 10)}>-10</button>
+        <button onClick={() => this.handleMathClick("substraction", 1)}>-1</button>
+        <button onClick={this.handleMathClick.bind(this,"reset")}>reset</button>
+        <button onClick={this.handleMathClick.bind(this,"addition", 1)}>+1</button>
+        <button onClick={this.handleMathClick.bind(this,"addition", 10)}>+10</button>
+        <h1>Liczba kliknięć: {this.state.count}</h1>
+        <h1>Wynik: {this.state.result}</h1>
       </>
     )
   }
 
-
 }
 
-ReactDOM.render(<Message/>, document.getElementById('root'))
-
-
-
-{/* <p>{this.state.messageIsActive ? text : ""}</p> */}
+ReactDOM.render(<Counter result={0} />, document.getElementById('root'))
