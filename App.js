0a5c0a1be7b0a6599a2b644a8b0114f0ea29f7e8
s16503 <s16503@pjwstk.edu.pl>
2021-09-08 11:38:30 +0200

    3-16 analiza zmiany stanu

--- App.js
+++ App.js
@@ -3,24 +3,29 @@ class App extends React.Component {
     value:""
   }
 
-  handleResetClick = () => {
+  handleClick = () => {
     this.setState({
       value:""
     })
   }
 
-  handleInputChange = (e) => {
-    console.log(e.target.value)
+  handleChange = (e) => {
+    console.log("Zawartość w evencie: " + e.target.value)
+    console.log("Zawartość przed setState: " + this.state.value)
     this.setState({
       value: e.target.value
     })
-    
+
+    console.log("Zawartość value po setState: " + this.state.value)
   }
   render() {
+    console.log("Zawartość value w trakcie render: " + this.state.value)
+    //state zmienia się dopiero przy renderowaniu strony
+    
     return (
       <>
-      <input value={this.state.value} placeholder="wpisz coś" onChange={this.handleInputChange} type="text"/>
-      <button onClick={this.handleResetClick}>Reset</button>
+      <input value={this.state.value} placeholder="wpisz coś" onChange={this.handleChange} type="text"/>
+      <button onClick={this.handleClick}>Reset</button>
       <h1 className = "title">{this.state.value}</h1>
       </>
     )
