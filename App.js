bb493aacd6cddc05eccbcbb51fe938bdd8c65efd
s16503 <s16503@pjwstk.edu.pl>
2021-09-09 16:52:54 +0200

    3-20 MathButton component

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ class Counter extends React.Component{
     result: this.props.result
   }
 
-  handleMathClick(type, number = 1){
-    
-    if(type === "substraction"){
+  handleMathClick = (type, number = 1) => {
+  // debugger
+    if(type === "subtraction"){
       this.setState(prevState => ({
         count: prevState.count + 1,
         result: prevState.result - number
@@ -31,17 +31,49 @@ class Counter extends React.Component{
   render(){
     return(
       <>
-        <button onClick={this.handleMathClick.bind(this,"substraction", 10)}>-10</button>
-        <button onClick={() => this.handleMathClick("substraction", 1)}>-1</button>
-        <button onClick={this.handleMathClick.bind(this,"reset")}>reset</button>
-        <button onClick={this.handleMathClick.bind(this,"addition", 1)}>+1</button>
-        <button onClick={this.handleMathClick.bind(this,"addition", 10)}>+10</button>
+        <MathButton 
+        name="-10" 
+        number="10" 
+        type="subtraction" 
+        click={this.handleMathClick}
+        />
+        <MathButton 
+        name="-1" 
+        number="1" 
+        type="subtraction" 
+        click={this.handleMathClick}
+        />
+         <MathButton 
+        name="reset" 
+        type="reset" 
+        click={this.handleMathClick}
+        />
+        <MathButton 
+        name="+1" 
+        number="1" 
+        type="addition" 
+        click={this.handleMathClick}
+        />
+        <MathButton 
+        name="+10" 
+        number="10" 
+        type="addition" 
+        click={this.handleMathClick}
+        />
         <h1>Liczba kliknięć: {this.state.count}</h1>
         <h1>Wynik: {this.state.result}</h1>
       </>
     )
   }
 
+}
+
+const MathButton = (props) =>{
+  const number = parseInt(props.number)
+  return(
+    <button onClick={ () => props.click(props.type, number) }>{props.name}</button>
+  )
+  
 }
 
 ReactDOM.render(<Counter result={0} />, document.getElementById('root'))
