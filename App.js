0807c5088486404274377511fefea1fcb5e25d1f
s16503 <s16503@pjwstk.edu.pl>
2021-09-09 18:44:02 +0200

    wybór komponentu do wysw. za pomocą checkboxa

--- App.js
+++ App.js
@@ -1,86 +1,41 @@
-class Counter extends React.Component{
+const PositiveMessage = () => <p>Możesz obejrzeć ten film</p>
 
-  state={
-    count: 0,
-    result: this.props.result
+const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!</p>
+
+
+class CheckboxAgeConfirmation extends React.Component{
+
+  state = {
+    isConfirmed: false,
   }
 
-  handleMathClick = (type, number = 1) => {
-  // debugger
-    if(type === "subtraction"){
-      this.setState(prevState => ({
-        count: prevState.count + 1,
-        result: prevState.result - number
-      }))
-    }else if(type ==="reset"){
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: 0
-        }
-      ))
-    }else if(type === "addition"){
-      this.setState(prevState => ({
-        count: prevState.count + 1,
-        result: prevState.result + number
-    }))
-   }
+  handleCheckboxChange = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed
+    })
   }
-  
+
+  displayMessage = () => {
+    if(this.state.isConfirmed){
+      return <PositiveMessage/>
+    }
+    else{
+      return <NegativeMessage/>
+    }
+  }
 
   render(){
+    console.log(this.state.isConfirmed)
     return(
       <>
-        <MathButton 
-        name="-10" 
-        number="10" 
-        type="subtraction" 
-        click={this.handleMathClick}
-        />
-        <MathButton 
-        name="-1" 
-        number="1" 
-        type="subtraction" 
-        click={this.handleMathClick}
-        />
-         <MathButton 
-        name="reset" 
-        type="reset" 
-        click={this.handleMathClick}
-        />
-        <MathButton 
-        name="+1" 
-        number="1" 
-        type="addition" 
-        click={this.handleMathClick}
-        />
-        <MathButton 
-        name="+10" 
-        number="10" 
-        type="addition" 
-        click={this.handleMathClick}
-        />
-        <ResultPanel count={this.state.count} result={this.state.result} />
+      <h1>Kup bilet na horror roku!</h1>
+      <input type="checkbox" id="age" onChange={this.handleCheckboxChange} defaultchcked={this.state.isConfirmed}/>
+      <label htmlFor="age">Mam conajmniej 16 lat</label>
+      {this.displayMessage()}
       </>
     )
   }
-
-}
-
-const MathButton = (props) =>{
-  const number = parseInt(props.number)
-  return(
-    <button onClick={ () => props.click(props.type, number) }>{props.name}</button>
-  )
 }
 
 
-const ResultPanel = (props) => {
-  return(
-    <>
-      <h1>Liczba kliknięć: {props.count} {props.count > 10 ? <span>Przeciążenie!!!</span> : null}</h1>
-      <h1>Wynik: {props.result}</h1>
-    </>
-  )
-}
-ReactDOM.render(<Counter result={0} />, document.getElementById('root'))
+ReactDOM.render(<CheckboxAgeConfirmation/>, document.getElementById('root'))
