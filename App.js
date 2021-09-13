02e169e3509ee6688cbe453b39d59671813b9dbd
s16503 <s16503@pjwstk.edu.pl>
2021-09-13 16:39:59 +0200

    cz3 - wyszarzenie zera

--- App.js
+++ App.js
@@ -32,7 +32,9 @@ class App extends React.Component{
         disabled={this.state.shoppingCart ? false : true}
         onClick={this.handleRemoveFromCart}
         >-</button>
-        <span> {this.state.shoppingCart} </span>
+        <span style={ this.state.shoppingCart === 0 ?{
+          opacity: 0.3
+        } : {}}> {this.state.shoppingCart} </span>
         <button 
         disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} 
         onClick={this.handleAddToCart}
