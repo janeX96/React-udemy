45bd44c11f75dca9ffbe685ecc33a7d384d0bc0c
s16503 <s16503@pjwstk.edu.pl>
2021-09-13 16:16:45 +0200

    cz1 - dodawanie i usuwanie

--- App.js
+++ App.js
@@ -1,84 +1,38 @@
-const ValidationMessage = (props) => {
-  //destrukturyzacja
-  const {txt} = props
-  //
-
-  return(
-    <p>{txt}</p>
-  )
-}
-
-const OrderForm = (props) => {
-  const {submit, checked, change} = props;
-
-  return(
-     <form onSubmit={submit}>
-  <input type="checkbox" id="age" onChange={change} checked={checked}/>
-  <label htmlFor="age">Mam conajmniej 16 lat</label>
-  <br/>
-  <button type="submit">Kup bilet</button>
-</form>
-  )
- 
-}
-
-class TicketShop extends React.Component{
-
+class App extends React.Component{
   state = {
-    isConfirmed: false,
-    isFormSubmitted: false
+    availableProducts: 7,
+    shoppingCart: 0,
+
   }
 
-  handleCheckboxChange = () => {
+  handleRemoveFromCart = () => {
     this.setState({
-      isConfirmed: !this.state.isConfirmed,
-      isFormSubmitted: false
+      shoppingCart: this.state.shoppingCart -1
     })
   }
 
-  displayMessage = () => {
-    if(this.state.isFormSubmitted){
-        if(this.state.isConfirmed){
-        return <ValidationMessage txt="Możesz obejrzeć film"/>
-      }
-      else{
-        return <ValidationMessage txt="Nie możesz obejrzeć tego filmu!"/>
-      }
-    }
-    else{
-      return null;
-    }
-  }
-
-  handleFormSubmit= (e) => {
-    e.preventDefault()
-
-    if(!this.state.isFormSubmitted){
-       this.setState({
-      isFormSubmitted: true
-     })
-    }
-   
+  handleAddToCart = () => {
+    this.setState({
+      shoppingCart: this.state.shoppingCart +1
+    })
   }
 
   render(){
-    //destrukturyzacja, wyodrębnianie elementu
-    const {isConfirmed, isFormSubmitted} = this.state
-    //
-    console.log(isConfirmed)
-
     return(
-      <>
-        <h1>Kup bilet na horror roku!</h1>
-        <OrderForm change={this.handleCheckboxChange} 
-        submit={this.handleFormSubmit}
-        checked={isConfirmed}
-        />
-        {this.displayMessage()}
-      </>
+      <div>
+        <button 
+        disabled={this.state.shoppingCart ? false : true}
+        onClick={this.handleRemoveFromCart}
+        >-</button>
+        <span> {this.state.shoppingCart} </span>
+        <button 
+        disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} 
+        onClick={this.handleAddToCart}
+        >+</button>
+      </div>
+      
     )
   }
 }
 
-
-ReactDOM.render(<TicketShop/>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
