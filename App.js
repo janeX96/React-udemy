ebbfb68150ed0ac7891b3f143e56eb6d9d1d24d6
s16503 <s16503@pjwstk.edu.pl>
2021-09-14 15:27:26 +0200

    wysw. elementów tablicy jako listy, metoda map

--- App.js
+++ App.js
@@ -1,49 +1,16 @@
-class App extends React.Component{
+class ListItems extends React.Component{
   state = {
-    availableProducts: 7,
-    shoppingCart: 0,
-
+    items: ["jabłko","śliwka","gruszka"]
   }
 
-  handleRemoveFromCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart -1
-    })
-  }
-
-  handleAddToCart = () => {
-    this.setState({
-      shoppingCart: this.state.shoppingCart +1
-    })
-  }
-
-  handleBuy = () => {
-    this.setState({
-      availableProducts: this.state.availableProducts - this.state.shoppingCart,
-      shoppingCart: 0,
-    })
-  }
-
-
-  render(){
-    const {shoppingCart, availableProducts} = this.state
-    const style = shoppingCart === 0 ? {opacity: 0.3} : {}
-    return(
-      <div>
-        <button 
-        disabled={shoppingCart ? false : true}
-        onClick={this.handleRemoveFromCart}
-        >-</button>
-        <span style={ this.style}> {shoppingCart} </span>
-        <button 
-        disabled={shoppingCart === availableProducts ? true : false} 
-        onClick={this.handleAddToCart}
-        >+</button>
-        {shoppingCart >0 && <button onClick={this.handleBuy}>Kup</button>}
-      </div>
-      
+  render() {
+    return (
+      <ul>
+         {this.state.items.map( item => <li key={item}>{'owoc '+ item}</li>)}
+      </ul>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+
+ReactDOM.render(<ListItems/>, document.getElementById('root'))
