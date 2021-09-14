957bfab9d0487064785f688451942be76891bf62
s16503 <s16503@pjwstk.edu.pl>
2021-09-14 15:36:03 +0200

    5-39 template string

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+const Item = (props) => <li>{`owoc ${props.nazwa}`}</li>
+
+
 class ListItems extends React.Component{
   state = {
     items: ["jabłko","śliwka","gruszka"]
   }
 
   render() {
+    const Items = this.state.items.map( item => <Item key = {item} nazwa = {item}/>)
+    
     return (
       <ul>
-         {this.state.items.map( item => <li key={item}>{'owoc '+ item}</li>)}
+        {Items}
       </ul>
     )
   }
