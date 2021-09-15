cd35c3b2f843355a74486e0beb95c7a7cd6635b2
s16503 <s16503@pjwstk.edu.pl>
2021-09-15 17:28:18 +0200

    Header

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 class App extends React.Component{
   state={
+    items: [
+      { id: 1, name: "herbata", active: true },
+      { id: 1, name: "ziemniaki", active: false },
+      { id: 1, name: "kasza", active: true },
+      { id: 1, name: "zupa wodna", active: true },
+      { id: 1, name: "wrzątek", active: false },
+      { id: 1, name: "chleb", active: true },
+    ],
 
+  
   }
 
   render(){
     return(
-      <div>Menu</div>
+      <>
+        <Header items={this.state.items}/>
+        <ListItems/>
+      </>
     );
   }
 }
