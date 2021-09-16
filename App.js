fc33df7cb05ec42c57c847f3305fb904de56efa3
s16503 <s16503@pjwstk.edu.pl>
2021-09-16 14:41:19 +0200

    cz2 - optymalizacja

--- App.js
+++ App.js
@@ -1,17 +1,3 @@
-// const Dollars = (props) => (
-//   <div>Wartość w dolarach: {props.cash <=0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
-// )
-
-// const Euros = (props) => {
-//   const value = (props.cash / props.ratio).toFixed(2)
-//   return(
-//       <div>Wartość w euro: {props.cash <= 0 ? "" : value}</div>
-//   )
-// }
-
-
-
-//w jednym komponencie
 const Cash = (props) => {
   const value = (props.cash / props.ratio).toFixed(2)
   return(
@@ -22,9 +8,24 @@ const Cash = (props) => {
 class ExchangeCounter extends React.Component {
   state={
     amount: "",
-    ratioDollar: 3.6,
-    ratioEuro: 4.2
   }
+
+  currencies = [
+    {
+      id: 1,
+      name: 'dollar',
+      ratio: 3.6,
+      title: 'Wartość w dolarach:'
+    },
+    {id: 2,
+      name: 'euro',
+      ratio: 4.2,
+      title: 'Wartość w euro:'},
+    {id: 3,
+      name: 'pound',
+      ratio: 5.20,
+      title: 'Wartość w funtach:'}
+  ]
 
   handleChange = e => {
     this.setState({
@@ -33,7 +34,11 @@ class ExchangeCounter extends React.Component {
   }
 
   render() {
-    const {amount,ratioDollar,ratioEuro} = this.state
+    const {amount} = this.state
+
+    const calculators = this.currencies.map(currency => (
+      <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount}/>
+    ))
 
     return (
       <div class="app">
@@ -44,10 +49,9 @@ class ExchangeCounter extends React.Component {
           onChange={this.handleChange}
           />
         </label>
-        {/* <Dollars cash={amount} ratio={ratioDollar}/>
-        <Euros cash={amount} ratio={ratioEuro}/> */}
-        <Cash cash={amount} ratio={ratioDollar} title="wartosć w dolarach"/>
-        <Cash cash={amount} ratio={ratioEuro} title="wartosć w euro"/>
+        {/* <Cash cash={amount} ratio={ratioDollar} title="wartosć w dolarach"/>
+        <Cash cash={amount} ratio={ratioEuro} title="wartosć w euro"/> */}
+        {calculators}
       </div>
     )
   }
