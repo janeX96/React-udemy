a09cdf59ce89278ea1154a2193f6b9424b1dd3de
s16503 <s16503@pjwstk.edu.pl>
2021-09-16 14:07:40 +0200

    optymalizacja + poprawka

--- App.js
+++ App.js
@@ -30,25 +30,41 @@ class Form extends React.Component {
     })
   }
 
+
+  handleChange = e => {
+    if(e.target.type === "checkbox"){
+      this.setState({
+        [e.target.name] : e.target.checked
+      })
+    }
+    else{
+       this.setState({
+         [e.target.name] : e.target.value
+      })
+    }
+   
+  }
+
   render() {
     return (
       <div>
         <label>
           Podaj miasto
-          <input value={this.state.city} onChange={this.handleCityChange} type="text"/>
+          <input name="city" value={this.state.city} onChange={this.handleChange} type="text"/>
         </label>
         <br/>
         <label>Napisz coś o tym mieście
-          <textarea value={this.state.text} onChange={this.handleTextChange}></textarea>
+          <textarea name="text" value={this.state.text} onChange={this.handleChange}></textarea>
         </label>
         <br/>
         <label>
           Czy lubisz to miasto?
-          <input type="checkbox" checked={this.state.checked} onChange={this.handleIsLovedChange}/>
+          <input name="isLoved" type="checkbox" checked={this.state.isLoved} onChange={this.handleChange}/>
         </label>
+        <br/>
         <label>
           Ile razy byłęś w tym mieście?
-          <select value={this.state.number} onChange={this.handleVisitsNumberChange.bind(this)}>
+          <select name="number" value={this.state.number} onChange={this.handleChange}>
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
