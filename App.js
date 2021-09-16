4ea693c8581a0750784eb5777c812af5f548c3cd
s16503 <s16503@pjwstk.edu.pl>
2021-09-16 18:22:27 +0200

    na 5 - losowanie wróżby

--- App.js
+++ App.js
@@ -1,109 +1,39 @@
-const Cash = (props) => {
-  const value = (props.cash / props.ratio * props.price).toFixed(2)
-  return(
-      <div>{props.title} {props.cash <= 0 ? "" : value}</div>
-  )
-}
-
-class ExchangeCounter extends React.Component {
-  state={
-    amount: "",
-    product: "gas"
+class OmenGenerator extends React.Component{
+  state = {
+    omens: 
+    [
+      "pierwsza wróżba",
+      "druga wróżba",
+      "trzecia wróżba"
+    ],
+    drawn: "",
+    
   }
 
-  static defaultProps = {
-    currencies : [
-    {
-      id: 0,
-      name: 'zloty',
-      ratio: 1,
-      title: 'Wartość w złotych:'
-    },
-    {
-      id: 1,
-      name: 'dollar',
-      ratio: 3.6,
-      title: 'Wartość w dolarach:'
-    },
-    {id: 2,
-      name: 'euro',
-      ratio: 4.2,
-      title: 'Wartość w euro:'},
-    {id: 3,
-      name: 'pound',
-      ratio: 5.20,
-      title: 'Wartość w funtach:'}
-   ],
-   prices: {
-     electricity: 0.51,
-     gas: 4.76,
-     oranges: 3.79
-   }
-  }
+  handleRandomOmen = () => {
+   
+    const min = 0
+    const max = this.state.omens.length-1
   
-
-  handleChange = e => {
+    const random = parseInt(Math.floor(Math.random() * (max - min + 1)) + min);
+  
     this.setState({
-      amount: e.target.value
+      drawn: this.state.omens[random]
     })
   }
 
-  handleSelect = e => {
-    this.setState({
-      product: e.target.value,
-      amount: ""
-    })
-  }
-
-  insertSuffix(select){
-    if(select === "electricity"){
-      return <em>kWh</em>
-    }
-    else if(select === "gas"){
-      return <em>litrów</em>
-    }
-    else if(select === "oranges"){
-      return <em>kilogramów</em>
-    }
-    else{
-      return null
-    }
-  }
-
-  selectPrice(select){
-     return this.props.prices[select]
-  }
-
-  render() {
-    const {amount,product} = this.state
-    const price = this.selectPrice(product)
-
-    const calculators = this.props.currencies.map(currency => (
-      <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount} price={price}/>
-    ))
-
-    return (
-      <div class="app">
-        <label>Wybierz produkt:
-          <select value={product} onChange={this.handleSelect}>
-            <option value="electricity"> prąd</option>
-            <option value="gas">benzyna</option>
-            <option value="oranges">pomarańcze</option>
-          </select>
-        </label>
-        <br/>
-        <label>
-          <input 
-          type="number"
-          value={this.state.amount}
-          onChange={this.handleChange}
-          />
-          {this.insertSuffix(this.state.product)}
-        </label>
-        {calculators}
-      </div>
+  render(){
+    return(
+      <>
+      <button onClick={this.handleRandomOmen}>Zobacz wróżbę</button>
+      <br/>
+      <input type="text"/>
+      <button>Dodaj wróżbę</button>
+      <br/>
+      <h1>{this.state.drawn}</h1>
+      </>
     )
   }
 }
 
-ReactDOM.render(<ExchangeCounter/>, document.getElementById('root'))
+ReactDOM.render(<OmenGenerator/>, document.getElementById('root'))
