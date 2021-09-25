6a0e14629d62077a43193f6e52e109f9ef75b22b
s16503 <s16503@pjwstk.edu.pl>
2021-09-25 16:01:48 +0200

    pobieranie danych z dynamicznego źródła

--- CRA/example/src/components/App.js
+++ CRA/example/src/components/App.js
@@ -1,36 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
-import SwitchButton from './SwitchButton';
+
+//rosnąca pseudo baza danych
+const data = [
+  {
+    id: 1, 
+    title: "Wiadomość nr 1", 
+    body: "Zawartość wiadomości nr 1 ..."
+  },
+  {
+    id: 2, 
+    title: "Wiadomość nr 2", 
+    body: "Zawartość wiadomości nr 2 ..."
+  },
+  {
+    id: 3, 
+    title: "Wiadomość nr 3", 
+    body: "Zawartość wiadomości nr 3 ..."
+  },
+]
+
+setInterval(()=> {
+  const index = data.length + 1
+  data.push({
+    id: index, 
+    title: `Wiadomość nr ${index}`, 
+    body: `Zawartość wiadomości nr ${index} ...`
+  })
+  // console.log(data)
+}, 8000)
 
 class App extends Component {
   state = {
-    time: 0,
-    active: false
+   comments: [...data]
   }
 
-  handleClick = () => {
-    if(this.state.active){
-      clearInterval(this.idInterval)
-    }else{
-      this.idInterval= setInterval(()=> this.addSecond(), 1000)
+  getData = () => {
+    
+    if(this.state.comments.length !== data.length){
+      console.log("aktualizacja")
+      this.setState({
+      comments: [...data]
+      })
     }
-
-    this.setState({
-      active: !this.state.active
-    })
+    else{
+      console.log("takie same dane - brak aktualizacji")
+    }
   }
 
-  addSecond = () =>{
-    this.setState({
-      time: this.state.time +1
-    })
+  componentDidMount(){
+    this.idI= setInterval(this.getData, 2000)
   }
+
+  //w razie likwidacji komponentu
+  componentWillUnmount(){
+    clearInterval(this.idI)
+  }
+  
   render() {
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ))
+    // console.log(comments)
     return (
-      <>
-        <p>{this.state.time}</p>
-        <SwitchButton click={this.handleClick} active={this.state.active}/>
-      </>
+     <div className="App">
+       {comments.reverse()}
+     </div>
     );
   }
 }
