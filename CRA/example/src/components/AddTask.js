5a315709b14fa96dfa159be5e00e531095b7cea8
s16503 <s16503@pjwstk.edu.pl>
2021-09-28 20:20:56 +0200

    97- obsługa pola wyboru daty

--- CRA/example/src/components/AddTask.js
+++ CRA/example/src/components/AddTask.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './AddTask.css'
 
 class AddTask extends Component {
+    minDate = new Date().toISOString().slice(0,10)
     state = {
         text: "",
         checked: false,
-        date: "2021-09-28"
+        date: this.minDate
 
     }
+
+    handleDate = (e) => {
+        this.setState({
+            date : e.target.value
+        })
+    }
+
     render() { 
+      let maxDate = this.minDate.slice(0,4) * 1 + 1
+      maxDate = maxDate + "-12-31" // 2022-12-31
         return(
             <div className="form">
               <input 
@@ -19,7 +29,7 @@ class AddTask extends Component {
               <input type="checkbox" checked={this.state.checked} id="important"/>
                 <label htmlFor="important">Priorytet</label>
                 <label html="date">Do kiedy zrobić</label>
-                <input type="date" value={this.state.date} min="2021-09-28"></input>
+                <input type="date" onChange={this.handleDate} value={this.state.date} min={this.minDate} max={maxDate}></input>
                 <button>Dodaj</button>
             </div>
         )
