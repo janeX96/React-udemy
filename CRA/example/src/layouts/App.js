65dfd24e133e97ab7dd351bbffb2ba1d5cf5a41b
s16503 <s16503@pjwstk.edu.pl>
2021-09-29 19:00:24 +0200

    107- komponent główny, CSS

--- CRA/example/src/layouts/App.js
+++ CRA/example/src/layouts/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom'
-
+import Header from './Header';
+import Navigation from './Navigation';
+import Footer from './Footer';
+import Page from './Page';
 
 class App extends Component {
  
@@ -10,9 +13,25 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        .......
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            {<Header/>}
+          </header>
+          <main>
+            <aside>
+              {<Navigation/>}
+            </aside>
+            <section className="page">
+              {<Page/>}
+            </section>
+          </main>
+          <footer>
+            {<Footer/>}
+          </footer>
+        </div>
+      </Router>
+     
     );
   }
 }
