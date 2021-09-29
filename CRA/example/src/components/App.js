8728436284e56226742b529ba8b28f1931d46a83
s16503 <s16503@pjwstk.edu.pl>
2021-09-29 16:37:58 +0200

    105- strona 404 i Switch

--- CRA/example/src/components/App.js
+++ CRA/example/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 //npm install react-router-dom
-import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom'
+import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom'
 
 const Home = () => <h1>Strona startowa</h1>
 const News = () => <h1>Aktualności</h1>
 const Contact = () => <h1>Kontakt</h1>
 
+const ErrorPage = () => <h1>Strona nie istnieje</h1>
 class App extends Component {
  
 
@@ -34,9 +35,13 @@ class App extends Component {
           </nav>
         </header>
         <section>
-          <Route path="/" exact component={Home}/>
-          <Route path="/news" component={News}/>
-          <Route path="/contact" component={Contact}/>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/news" component={News}/>
+            <Route path="/contact" component={Contact}/>
+            <Route component={ErrorPage}/>
+          </Switch>
+         
         </section>
       </div>
     </Router>
