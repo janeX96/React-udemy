3de86527d4b74df5104d427f2c91f41fedcdfcd3
s16503 <s16503@pjwstk.edu.pl>
2021-09-29 22:48:50 +0200

    115- stopka

--- CRA/example/src/layouts/Footer.js
+++ CRA/example/src/layouts/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
-const Footer = () => {
+import { Route } from 'react-router';
+import '../styles/Footer.css'
+const Footer = (props) => {
     return ( 
     <div>
-        Footer
+        <h2>Stopka</h2>
+        <Route path="/" exact render={(props)=> (
+                <p>Jesteś na <span>stronie głównej</span></p>
+            )
+        }/>
+
+        <Route path="/:page/:idProduct" exact render={(props)=> {
+            console.log(props)
+            return(
+                <>
+                 <p>Jesteś na <span>{props.match.params.idProduct}</span></p>
+                <p>Jesteś na <span>{props.match.url}</span></p>
+                <p>Jesteś na <span>{props.match.path}</span></p>
+                </>
+            )
+        }}/>
     </div> 
     );
 }
