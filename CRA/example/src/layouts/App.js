96be0f6b9c7361274910ff950cc79384d170921b
s16503 <s16503@pjwstk.edu.pl>
2021-09-29 16:52:21 +0200

    106- struktura katalogów

---
+++ CRA/example/src/layouts/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom'
+
+
+class App extends Component {
+ 
+
+ 
+
+  render() {
+    return (
+      <div>
+        .......
+      </div>
+    );
+  }
+}
+
+
+export default App;
