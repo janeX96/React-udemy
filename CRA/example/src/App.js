761859d911470b5d11c15a3a65e667f794d88498
s16503 <s16503@pjwstk.edu.pl>
2021-10-26 12:33:27 +0200

    cz2- dynamiczne dodawanie klas, klasy w tablicy

--- CRA/example/src/App.js
+++ CRA/example/src/App.js
@@ -2,10 +2,32 @@ import React, { Component } from "react";
 import "./App.css";
 import Text from "./Text";
 class App extends Component {
+  state = {
+    underline: false,
+  };
   render() {
+    const text = "Hello!";
+    // let classes = "";
+    // if (this.state.underline) {
+    //   classes += " line";
+    // }
+    // if (text.length > 10) {
+    //   classes += " big";
+    // }
+
+    const classes = ["one"];
+    if (this.state.underline) {
+      classes.push("line");
+    }
+    if (text.length > 10) {
+      classes.push("big");
+    }
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div
+        className="App"
+        onClick={() => this.setState({ underline: !this.state.underline })}
+      >
+        <h1 className={classes.join(" ")}>{text}</h1>
         <Text />
       </div>
     );
