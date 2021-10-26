97f797bf8b8ddbf52ac1c9864c60f4bdd7c2f289
s16503 <s16503@pjwstk.edu.pl>
2021-10-26 12:44:06 +0200

    npm install bootstrap , przycisk z bootstrapa

--- CRA/example/src/Text.js
+++ CRA/example/src/Text.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Text.css";
 const Text = () => {
   return (
-    <p className="red">
-      Lorem oopsum ,sdasa ,sadas3greg, dfewfe wda. Ddadasd grg ,df dfeeef ,fds
-      fefds.
-    </p>
+    <div>
+      <button className="btn btn-primary">Przycisk</button>
+      <p className="red">
+        Lorem oopsum ,sdasa ,sadas3greg, dfewfe wda. Ddadasd grg ,df dfeeef ,fds
+        fefds.
+      </p>
+    </div>
   );
 };
 
