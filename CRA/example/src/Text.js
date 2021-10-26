c0a9f4289852341ee9f5a96ab295e8f2d6bab282
s16503 <s16503@pjwstk.edu.pl>
2021-10-26 12:20:25 +0200

    cz1

---
+++ CRA/example/src/Text.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Text.css";
+const Text = () => {
+  return (
+    <p className="red">
+      Lorem oopsum ,sdasa ,sadas3greg, dfewfe wda. Ddadasd grg ,df dfeeef ,fds
+      fefds.
+    </p>
+  );
+};
+
+export default Text;
