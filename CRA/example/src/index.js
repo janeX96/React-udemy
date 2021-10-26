42ba545f22a1ead59038b2907045737bef5bd5e4
s16503 <s16503@pjwstk.edu.pl>
2021-10-26 13:36:51 +0200

    lista zakupów z sekcji 3

--- CRA/example/src/index.js
+++ CRA/example/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import ShoppingList from "./ShoppingList";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ShoppingList />, document.getElementById("root"));
