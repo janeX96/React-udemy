c333b8d5be6924713cdb808ebbc2124399025fd5
s16503 <s16503@pjwstk.edu.pl>
2021-10-26 12:09:59 +0200

    stylowanie liniowe

---
+++ CRA/example/src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    active: false,
+  };
+
+  render() {
+    // const btn_off = {
+    //   border: "2px solid black",
+    //   padding: "10px 20px",
+    //   fontFamily: "arial",
+    //   fontSize: 30,
+    //   display: "block",
+    //   margin: "20px auto",
+    //   background: "white",
+    // };
+
+    // const btn_on = {
+    //   border: "2px solid black",
+    //   padding: "10px 20px",
+    //   fontFamily: "arial",
+    //   fontSize: 30,
+    //   display: "block",
+    //   margin: "20px auto",
+    //   background: "cadetblue",
+    //   color: "white",
+    // };
+
+    // const styles = {
+    //   btn_off: {
+    //     border: "2px solid black",
+    //     padding: "10px 20px",
+    //     fontFamily: "arial",
+    //     fontSize: 30,
+    //     display: "block",
+    //     margin: "20px auto",
+    //     background: "white",
+    //   },
+    //   btn_on: {
+    //     border: "2px solid black",
+    //     padding: "10px 20px",
+    //     fontFamily: "arial",
+    //     fontSize: 30,
+    //     display: "block",
+    //     margin: "20px auto",
+    //     background: "cadetblue",
+    //     color: "white",
+    //   },
+    // };
+
+    let btn = {
+      border: "2px solid black",
+      padding: "10px 20px",
+      fontFamily: "arial",
+      fontSize: 30,
+      display: "block",
+      margin: "20px auto",
+      backgroundColor: "white",
+    };
+
+    if (this.state.active) {
+      btn.backgroundColor = "cadetblue";
+      btn.color = "white";
+    }
+    return (
+      <div>
+        <button
+          style={btn}
+          onClick={() => this.setState({ active: !this.state.active })}
+        >
+          {this.state.active ? "Wyłącz" : "Włącz"}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
--- CRA/example/src/pages/ErrorPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => {
-    return ( <div>Nie ma takiej strony</div> );
-}
- 
-export default ErrorPage;
